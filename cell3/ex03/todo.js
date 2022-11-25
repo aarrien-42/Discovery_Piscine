c0a4f015944e3f89367ed43e45bd955329230914
Andoni Arrien Martin <aarrien-@c2r1s5.42urduliz.com>
2022-11-25 17:19:25 +0100

    Piscine-Discovery

--- cell3/ex03/todo.js
+++ cell3/ex03/todo.js
@@ -6,10 +6,52 @@ function createToDo() {
 		tarea.setAttribute('onclick', 'deleteToDo()');
 		tarea.innerHTML = tareaTexto;
 		document.getElementById("ft_list").prepend(tarea); /* append lo pone al final */
+		addCookie(tareaTexto);
 	}
 }
+
 function deleteToDo() {
 	confirmation = confirm("¿Está seguro de que desea borrar la tarea?");
+	if (document.cookie){
+		var splitted = document.cookie.split("_");
+		document.cookie = document.cookie + ";max-age=0";
+	}
+	var i = 0;
+	var finalCookies;
 	if (confirmation == true)
+	{
+		console.log("target.content = ", event.target.innerHTML);
+		while (splitted[i])
+		{
+			if (event.target.innerHTML != splitted[i])
+			{
+				if (finalCookies == "")
+					document.cookie = splitted[i] + "_";
+				else
+					document.cookie = document.cookie + splitted[i] + "_";
+			}
+			i++;
+		}
 		event.target.remove();
+	}
+}
+
+function addCookie(content) {
+	if (!document.cookie)
+		document.cookie = tareaTexto + "_";
+	else
+		document.cookie = document.cookie + tareaTexto + '_';
+}
+
+function checkCookies() {
+	var i = 0;
+	var splitted = document.cookie.split("_");
+	while (splitted[i]) {
+		let tarea = document.createElement("div");
+		tarea.classList.add('tarea');
+		tarea.setAttribute('onclick', 'deleteToDo()');
+		tarea.innerHTML = splitted[i];
+		document.getElementById("ft_list").prepend(tarea);
+		i++;
+	}
 }
