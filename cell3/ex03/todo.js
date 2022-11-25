7215abaa25ed9c77196a8a94e88626376e08ae62
Andoni Arrien Martin <aarrien-@c2r1s5.42urduliz.com>
2022-11-25 16:14:29 +0100

    Piscine-Discovery

--- cell3/ex03/todo.js
+++ cell3/ex03/todo.js
@@ -1,1 +1,15 @@
-
+function createToDo() {
+	tareaTexto = window.prompt("Tarea:");
+	if (tareaTexto != "") {
+		let tarea = document.createElement("div");
+		tarea.classList.add('tarea'); /* se puede hacer con setAttribute */
+		tarea.setAttribute('onclick', 'deleteToDo()');
+		tarea.innerHTML = tareaTexto;
+		document.getElementById("ft_list").prepend(tarea); /* append lo pone al final */
+	}
+}
+function deleteToDo() {
+	confirmation = confirm("¿Está seguro de que desea borrar la tarea?");
+	if (confirmation == true)
+		event.target.remove();
+}
